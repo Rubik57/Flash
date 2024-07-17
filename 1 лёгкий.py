9ab545f9f745fd7b7e1af78496bf59f15f4cd8aa
Rubik57 <rubik0700@gmail.com>
2024-07-17 16:06:36 +0500

    Add files via upload

---
+++ 1 лёгкий.py
@@ -0,0 +1,17 @@
+def vsed(n):
+    a = []
+    if n**0.5==int(n**0.5):
+        for d in range(2, int(n**0.5)):
+            if n%d==0:
+                a.append(d)
+    if len(a)==1:
+        return a[0]
+    else:
+        return 0
+
+for i in range(400000,2000000+1):
+    s = vsed(i)
+    if s!=0:
+        print(i, s)
+    
+            

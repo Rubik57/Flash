8784bfeff261240b8a073042d34237de1cf54f46
Rubik57 <rubik0700@gmail.com>
2024-07-22 20:10:15 +0500

    Delete 2 средний.py

--- 2 средний.py
+++
@@ -1,7 +0,0 @@
-maxs = 0
-for n in range(0, 50 + 1, 2):
-    for m in range(1, 50 + 1, 2):
-        A = 5**n + 3**m
-        if m%10==5 and n%10!=6 and A%13==0:
-            maxs = max(maxs, n+m)
-print(maxs)
